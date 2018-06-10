3681a082b4e9860a42ec240da5c9282d305682df
RellyRivlin <38886884+RellyRivlin@users.noreply.github.com>
2018-06-10 15:14:15 +0300

    emit before request
    emit before request enables you to change the request options

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ function logBodyChunk(array, chunk) {
 
 
 function attachLoggersToRequest(protocol, options, callback) {
+  globalLogSingleton.emit('before', protocol, options);
   let self = this;
   let req = ORIGINALS[protocol].request.call(self, options, callback);
 
